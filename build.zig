676a0888b90b04659fda8f9b7d43a5ab98af3a37
banteg <4562643+banteg@users.noreply.github.com>
2026-04-30 18:10:34 +0400

    test: add msgpack conformance vectors (#17)

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("msgpack", .{
+    const msgpack_module = b.addModule("msgpack", .{
         .root_source_file = b.path("src/msgpack.zig"),
     });
 
@@ -18,6 +18,20 @@ pub fn build(b: *std.Build) !void {
 
     const run_test = b.addRunArtifact(tests);
 
+    const full_conformance_test_module = b.createModule(.{
+        .root_source_file = b.path("tests/full_conformance_vectors.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    full_conformance_test_module.addImport("msgpack", msgpack_module);
+
+    const full_conformance_tests = b.addTest(.{
+        .root_module = full_conformance_test_module,
+    });
+
+    const run_full_conformance_tests = b.addRunArtifact(full_conformance_tests);
+
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_test.step);
+    test_step.dependOn(&run_full_conformance_tests.step);
 }
